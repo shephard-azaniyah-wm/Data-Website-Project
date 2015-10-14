2bf7cd511f7dae7528d11eae663b060171a22fa1
Azaniyah Shephard <session1@START-Coding-MBP-C02PP4G9G8WN.local>
2015-10-14 15:12:23 -0700

    Finished adding the first set of data.

--- Graph.js
+++ Graph.js
@@ -1,37 +1,39 @@
 /**
  * Created by session1 on 10/9/15.
  */
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+    google.load("visualization", "1.1", {packages:["bar"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Year', 'Dogs', 'Cats', 'Birds'],
-        ['2011', 35, 400, 200],
-        ['2012', 1170, 460, 250],
-        ['2013', 660, 1120, 300]
+        ['2011', 133, 56, 6],
+        ['2012', 2, 0, 0],
+        ['2013', 6, 11, 0]
     ]);
 
     var options = {
         chart: {
-            title: 'Company Performance',
+            title: 'Harbor Shelter',
             subtitle: 'Animals taken into Harbor: 2011-2013',
         },
-        bars: 'horizontal', // Required for Material Bar Charts.
-        hAxis: {format: 'decimal'},
-        height: 400,
-        colors: ['#1b9e77', '#d95f02', '#7570b3']
+        bars: 'horizontal' // Required for Material Bar Charts.
     };
 
-    var chart = new google.charts.Bar(document.getElementById('chart_div'));
+    var chart = new google.charts.Bar(document.getElementById('barchart_material'));
 
-    chart.draw(data, google.charts.Bar.convertOptions(options));
-
-    var btns = document.getElementById('btn-group');
-
-    btns.onclick = function (e) {
-
-        if (e.target.tagName === 'BUTTON') {
-            options.hAxis.format = e.target.id === 'none' ? '' : e.target.id;
-            chart.draw(data, google.charts.Bar.convertOptions(options));
-        }
-    }
+    chart.draw(data, options);
 }
+</script>
+</head>
+<body>
+<div id="barchart_material" style="width: 900px; height: 500px;"></div>
+    </body>
+
+
+
+    </html>
