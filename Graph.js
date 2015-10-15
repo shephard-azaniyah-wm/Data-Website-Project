b8a65ac0ffa6ebf0c2805dcf2d27dcbc6d352820
Azaniyah Shephard <session1@START-Coding-MBP-C02PP4G9G8WN.local>
2015-10-15 15:18:07 -0700

    Both Graphs are not visible at the same time.

--- Graph.js
+++ Graph.js
@@ -1,12 +1,4 @@
-/**
- * Created by session1 on 10/9/15.
- */
-<!DOCTYPE html>
-<html>
-<head>
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-    google.load("visualization", "1.1", {packages:["bar"]});
+google.load("visualization", "1.1", {packages:["bar"]});
 google.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
@@ -19,21 +11,11 @@ function drawChart() {
     var options = {
         chart: {
             title: 'Harbor Shelter',
-            subtitle: 'Animals taken into Harbor: 2011-2013',
+            subtitle: 'Animals taken into Harbor: 2011-2013'
         },
         bars: 'horizontal' // Required for Material Bar Charts.
     };
-
-    var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+    var chart = new google.charts.Bar(document.getElementById('chart_div'));
 
     chart.draw(data, options);
 }
-</script>
-</head>
-<body>
-<div id="barchart_material" style="width: 900px; height: 500px;"></div>
-    </body>
-
-
-
-    </html>
---
+++ Graph2.js
@@ -0,0 +1,28 @@
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses', 'Profit'],
+        ['2014', 1000, 400, 200],
+        ['2015', 1170, 460, 250],
+        ['2016', 660, 1120, 300],
+        ['2017', 1030, 540, 350]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        },
+        bars: 'vertical',
+        vAxis: {format: 'decimal'},
+        height: 400,
+        colors: ['#1b9e77', '#d95f02', '#7570b3']
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('chart2_div'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+
+
+}
+
